69f2b9ea4e3e2ca28317cad511301fc8df3dcb6e
Nishi Lad <nishilad2208@gmail.com>
2019-10-20 18:33:49 -0500

    Headline Component and test

--- src/App.js
+++ src/App.js
@@ -1,11 +1,16 @@
 import React from "react";
 import Header from "./component/header";
+import Headline from './component/headline';
 import './app.scss';
 
 function App() {
   return (
     <div className="App">
       <Header />
+      <section className='main'>
+      <Headline header='posts' desc='Click button to render posts'/>
+      </section>
+     
     </div>
   );
 }
